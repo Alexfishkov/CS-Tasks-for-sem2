2133fd79238fc406c663a6d65e2e1e0ddbf1ef36
Alexfishkov <afscorp@mail.ru>
2022-12-05 19:50:08 +0300

    Define weekend day by number

---
+++ Task 15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Определяем по номеру дня недели, является ли этот день выходным");
+Console.Write("Введите номер дня недели=>");
+int yournumber = Int32.Parse(Console.ReadLine());
+
+if ((yournumber <= 7) && (yournumber >= 1))
+{
+    if ((yournumber <= 7) && (yournumber >= 6)) Console.WriteLine("Это выходной день :)");
+    else Console.WriteLine("Это не выходной день");
+}
+
+else Console.WriteLine("Дня недели с таким номером нет!");
